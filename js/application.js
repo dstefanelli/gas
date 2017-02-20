8162b807a5db9a13facdf95c986f861e8ccb0922
alesm01 <alejandro.d.mertens@accenture.com>
2017-02-20 11:42:42 -0300

    Subiendo cambios

--- js/application.js
+++ js/application.js
@@ -1,128 +1,22 @@
 $(function () {
-		var $body = $('body'),
-			$thirdM = $('.second-level-navigation.active .metro-menu > div > .column'),
-			$titleM = $('.metro-menu > div > .column h3');
-		$('.open-menu').on('click', function(){
-			var $this = $(this);
-			$this.parent().toggleClass('active').siblings('.nav-main').toggleClass('active');
-			$body.toggleClass('hidde-scroll');
-		});
-		$('.first-level').on('click', function(e){
-			e.preventDefault();
-			var $this = $(this),
-				query = $this.data('query');
-				console.log(query)
-			$this.parent().find('#' + query ).toggleClass("active");
-			$this.parent().siblings().find(".second-level-navigation").removeClass("active");
-		});
-	
-		$('.second-level').on('click', function(e){
-			e.preventDefault();
-			var $this = $(this),
-				query = $this.data('query');
-				console.log(query)
-			$this.parent().find('#' + query ).slideToggle();
-			$this.parent().siblings().find(".third-level-navigation").slideUp();
-		});
-		// EQUALIZE MENU COLUMNS
-	    $(document).ready(function () {
-		   equalheight( $thirdM );
-		   equalheight( $titleM );
-			console.log("entrando al equialize")
-		});
-		$(window).resize(function () {
-			equalheight( $thirdM );
-		    equalheight( $titleM );
-			console.log("rezise al equialize")
-		});
-	    
-});
-
-/*var METROGAS = function(){
-	
-	init = function () {
-		
-    	var $body = $('body');
-		
-		$('.open-menu').on('click', function(){
-			var $this = $(this);
-			$this.parent().toggleClass('active').siblings('.nav-main').toggleClass('active');
-			$body.toggleClass('hidde-scroll');
-		});
-		
-		$('.first-level').on('click', function(e){
-			e.preventDefault();
-			var $this = $(this),
-				query = $this.data('query');
-				console.log(query)
-			$this.parent().find('#' + query ).toggleClass("active");
-			$this.parent().siblings().find(".second-level-navigation").removeClass("active");
-		});
-	
-		$('.second-level').on('click', function(e){
-			e.preventDefault();
-			var $this = $(this),
-				query = $this.data('query');
-				console.log(query)
-			$this.parent().find('#' + query ).slideToggle();
-			$this.parent().siblings().find(".third-level-navigation").slideUp();
-		});
-    },
-		
-	equalheight = function (container) {
-
-		var currentTallest = 0,
-			currentRowStart = 0,
-			rowDivs = new Array(),
-			$el,
-			topPosition = 0;
-		$(container).each(function () {
-
-			$el = $(this);
-			$($el).height('auto')
-			topPostion = $el.position().top;
-
-			if (currentRowStart != topPostion) {
-				for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
-					rowDivs[currentDiv].height(currentTallest);
-				}
-				rowDivs.length = 0; // empty the array
-				currentRowStart = topPostion;
-				currentTallest = $el.height();
-				rowDivs.push($el);
-			} else {
-				rowDivs.push($el);
-				currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
-			}
-			for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
-				rowDivs[currentDiv].height(currentTallest);
-			}
-		});
-	}	
-	
-	return{
-		init: init
-	};
-	$(function () {
 
     var breakpoint = $(window).width(),
         $body = $('body'),
-        $ribbon = $('#s4-ribbonrow'),
-        tempRibbonH = $ribbon.height(),
+        isRibbon = $('#s4-ribbonrow:visible'),
         $navMain = $('.nav-main'),
+		$secondNav = $('.second-level-navigation'),
+		$secondLink = $('.second-level-navigation > li > a'),
         $thirdM = $('.third-level-navigation.active .metro-menu > div > .column'),
-		$titleM = $('.metro-menu > div > .column h3');
+        Popup = {};
 
     //  POPUPS
-    var Popup = function () {
-        open = function (id, callback) {
-            $("#" + id).addClass("active");
-            $('body').addClass("hidde-scroll");
-        }
-        close = function (id, callback) {
-            $("#" + id).removeClass("active");
-            $('body').removeClass("hidde-scroll");
-        }
+    Popup.open = function (id) {
+        $("#" + id).addClass("active");
+        $('body').addClass("hidde-scroll");
+    }
+    Popup.close = function (id) {
+        $("#" + id).removeClass("active");
+        $('body').removeClass("hidde-scroll");
     }
 
     //  OPEN MOBILE NAVIGATION
@@ -133,48 +27,49 @@ $(function () {
     });
 
     // OPEN FIRST LEVEL NAVIGATION
-    $('.first-level').on('click', function (e) {
+    $('body').on('click', '.first-level', function (e) {
         e.preventDefault();
         var $this = $(this),
-				query = $this.data('query');
-
-        equalheight($thirdM);
-        equalheight($titleM);
-
+			query = $this.data('query');  
+		//$secondLink.removeClass('active');
         $this.parent().find('#' + query).toggleClass("active");
-        $this.parent().siblings().find(".second-level-navigation").removeClass("active");
+		
+        $this.parents().siblings().find($secondNav).removeClass("active");
+        $this.parents().siblings().find($secondLink).removeClass("active");
     });
 
     // OPEN SECOND LEVEL NAVIGATION
-    $('.second-level').on('click', function (e) {
+    $('body').on('click', '.second-level', function (e) {
         e.preventDefault();
         var $this = $(this),
-				query = $this.data('query');
+			query = $this.data('query');
 
-        equalheight($thirdM);
-        equalheight($titleM);
-
-        $this.parent().find('#' + query).slideToggle();
-        $this.parent().siblings().find(".third-level-navigation").slideUp();
+        $this.addClass('active');
+        $this.parent().find('#' + query).slideDown("fast");
+		$this.parents().siblings().find('.second-level').removeClass('active');
+        $this.parents().siblings().find(".third-level-navigation").slideUp("fast");
     });
-
+	
     $(window).on("load resize", function (e) {
 
         // EQUALIZE MENU COLUMNS
         equalheight($thirdM);
-        equalheight($titleM);
 
         // MOBILE MENU POSITION
-        if (breakpoint >= 1110) {
-            $navMain.css("top", "auto");
-        }
-        else if (breakpoint <= 1110 && $ribbon.length > 0) {
-            $navMain.css("top", "94px");
-        }
-        else if (breakpoint <= 1110 && $ribbon.length < 0) {
-            $navMain.css("top", "50px");
-        }
+		if(isRibbon){
+			if (breakpoint >= 1110) {
+				$navMain.css("top", "auto");
+			}
+			else if (breakpoint <= 1109) {
+				$navMain.css("top", "94px");
+			}
+		}else{
+			if (breakpoint <= 1109) {
+				$navMain.css("top", "50px");
+			}else if(breakpoint >= 1110){
+				$navMain.css("top", "auto");
+			}
+		}
 
-    });
+    }); 
 });
-}();*/
